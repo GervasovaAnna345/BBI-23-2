25706595f546d5a064c3822e62ef2fe45b44b5c5
GervasovaAnna345 <159702403+GervasovaAnna345@users.noreply.github.com>
2024-05-30 15:46:41 +0300

    Add files via upload

---
+++ Serializarot/Serializator.cs
@@ -0,0 +1,74 @@
+﻿
+using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+namespace LR9_1.Serialize
+{
+    public abstract class Serialize
+    {
+        public abstract T Read<T>(string filePath);
+        public abstract void Write<T>(T obj, string filePath);
+    }
+
+    public class Xml : Serialize
+    {
+        public override T Read<T>(string filePath)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                return (T)serializer.Deserialize(fs);
+
+            }
+        }
+        public override void Write<T>(T obj, string filePath)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                serializer.Serialize(fs, obj);
+            }
+        }
+    }
+
+    public class Json : Serialize
+    {
+        public override T Read<T>(string filePath)
+        {
+            using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                return JsonSerializer.Deserialize<T>(fs);
+            }
+        }
+        public override void Write<T>(T obj, string filePath)
+        {
+            using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                JsonSerializer.Serialize<T>(fs, obj);
+            }
+        }
+    }
+    public class Bin : Serialize
+    {
+        public override T Read<T>(string filePath)
+        {
+            using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                return Serializer.Deserialize<T>(fs);
+            }
+        }
+
+        public override void Write<T>(T obj, string filePath)
+        {
+            using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                Serializer.Serialize(fs, obj);
+            }
+        }
+    }
+}
